2f21f9622b25a410d0944a7c488ddaf457e8afc9
butamapg <110253181+butamapg@users.noreply.github.com>
2022-10-27 04:01:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 let read = document.querySelector('.read-more');
-let active = document.querySelector('.none');
 
 
 read.addEventListener('click', (e) => {
 
 		
 			
-			if(read.className === "read-more") {
-				read.innerHTML = "";
-				read.innerHTML = "Shopee merupakan sebuah aplikasi yang bergerak di bidang jual beli secara online dan dalam bentuk aplikasi yang memudahkan penggunaanya untuk melakukan kegiatan berbelanja secara online tanpa harus ribet menggunakan perangkat komputer karena dapat di akses secara mudah dengan menggunakan smartphone. Shopee menawarkan berbagai macam produk, dari fashion hingga produk kebutuhan sehari-hari.";
-				read.style.color = "black"
+	if(read.className === "read-more") {
+	   read.innerHTML = "";
+	   read.innerHTML = "Shopee merupakan sebuah aplikasi yang bergerak di bidang jual beli secara online dan dalam bentuk aplikasi yang memudahkan penggunaanya untuk melakukan kegiatan berbelanja secara online tanpa harus ribet menggunakan perangkat komputer karena dapat di akses secara mudah dengan menggunakan smartphone. Shopee menawarkan berbagai macam produk, dari fashion hingga produk kebutuhan sehari-hari.";
+	   read.style.color = "black"
 										
 
 
@@ -18,7 +17,3 @@ read.addEventListener('click', (e) => {
 		
 
 });
-
-
-
-// 
